90447d4897b1031cc3c951144fcfe3d547ff8a2e
avjasso <avjasso@gmail.com>
2021-08-06 18:31:46 -0500

    Initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alexv/Devs/djangoCode/rathan_tuts/carzone-project/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
